f848d0947554039ded1c6af6ab938169c19133c6
Luka Nestorovic <lukanestorovic41@gmail.com>
2024-08-06 15:05:44 +0200

    JWT Branch

--- be/routes/deleteUser.js
+++ be/routes/deleteUser.js
@@ -4,10 +4,8 @@ const verifyJWT = require("../middlewares/middlewares");
 
 router.delete("/register/:id", verifyJWT, (req, res) => {
   if (req.data.id == req.params.id) {
-    console.log("DELETED");
     res.status(200).json("User has been deleted");
   } else {
-    console.log("YOU CANT DELETE ME");
     res.status(403).json("You are not allowed to delete this user.");
   }
 });
